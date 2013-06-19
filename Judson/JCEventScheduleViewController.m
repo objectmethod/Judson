1f56481b1077cd6891139854a47ebc6791b88f5a
richardpaulguy@gmail.com <richardpaulguy@gmail.com>
2013-06-19 17:25:53 -0400

    refactored scheudle to dynamic

--- Judson/JCEventScheduleViewController.m
+++ Judson/JCEventScheduleViewController.m
@@ -2,129 +2,141 @@
 
 @interface JCEventScheduleViewController ()
 
+@property (strong, nonatomic) NSArray *days;
+
 @end
 
 @implementation JCEventScheduleViewController
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-	[super tableView:tableView didSelectRowAtIndexPath:indexPath];
+- (void) viewDidLoad {
+	[super viewDidLoad];
 	
-	switch (indexPath.section) {
+	self.days = @[
+			   @{
+		  @"title" : @"Legends Pro-College-Am, 18 Holes",
+	@"startDate" : @{
+			@"day" : [NSNumber numberWithInt:12],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:11],
+   @"minute" : [NSNumber numberWithInt:0]},
+	@"endDate" : @{
+			@"day" : [NSNumber numberWithInt:12],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:17],
+   @"minute" : [NSNumber numberWithInt:0]}},
+	  @{
+		  @"title" : @"Legends Pro-College Stroke & Best Ball Challenge",
+	@"startDate" : @{
+			@"day" : [NSNumber numberWithInt:13],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:8],
+   @"minute" : [NSNumber numberWithInt:0]},
+	@"endDate" : @{
+			@"day" : [NSNumber numberWithInt:13],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:14],
+   @"minute" : [NSNumber numberWithInt:0]}},
+	  @{
+		  @"title" : @"Collegiate Stroke Play Second Round",
+	@"startDate" : @{
+			@"day" : [NSNumber numberWithInt:14],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:7],
+   @"minute" : [NSNumber numberWithInt:30]},
+	@"endDate" : @{
+			@"day" : [NSNumber numberWithInt:14],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:13],
+   @"minute" : [NSNumber numberWithInt:30]}},
+	  @{
+		  @"title" : @"Collegiate Stroke Play Final Round",
+	@"startDate" : @{
+			@"day" : [NSNumber numberWithInt:15],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:7],
+   @"minute" : [NSNumber numberWithInt:30]},
+	@"endDate" : @{
+			@"day" : [NSNumber numberWithInt:15],
+   @"month" : [NSNumber numberWithInt:7],
+   @"year" : [NSNumber numberWithInt:2013],
+   @"hour" : [NSNumber numberWithInt:13],
+   @"minute" : [NSNumber numberWithInt:30]}}
+	  ];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return self.days.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	static NSString *CellIdentifier = @"EventCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+	
+	NSDictionary *event = [self.days objectAtIndex:indexPath.section];
+	
+	NSString *eventStartDateString = [self dateStringFromEvent:[event objectForKey:@"startDate"]];
+	NSString *eventName = [event objectForKey:@"title"];
+	
+	switch (indexPath.row) {
 		case 0:
-			switch (indexPath.row) {
-				case 0:
-				{
-					NSDateComponents *startComponents = [[NSDateComponents alloc] init];
-					[startComponents setDay:12];
-					[startComponents setMonth:7];
-					[startComponents setYear:2013];
-					[startComponents setHour:11];
-					[startComponents setMinute:0];
-					NSDate *startDate = [[NSCalendar currentCalendar] dateFromComponents:startComponents];
-					
-					NSDateComponents *endComponents = [[NSDateComponents alloc] init];
-					[endComponents setDay:12];
-					[endComponents setMonth:7];
-					[endComponents setYear:2013];
-					[endComponents setHour:17];
-					[endComponents setMinute:0];
-					NSDate *endDate = [[NSCalendar currentCalendar] dateFromComponents:endComponents];
-					
-					[self promptToAddEventToCalendarWithTitle:@"Legends Pro-College-Am,18 Holes" startDate:startDate endDate:endDate allDay:NO];
-					
-					break;
-				}
-				default:
-					break;
-			}
+			cell.textLabel.text = eventStartDateString;
 			break;
 		case 1:
-			switch (indexPath.row) {
-				case 0:
-				{
-					NSDateComponents *startComponents = [[NSDateComponents alloc] init];
-					[startComponents setDay:13];
-					[startComponents setMonth:7];
-					[startComponents setYear:2013];
-					[startComponents setHour:8];
-					[startComponents setMinute:0];
-					NSDate *startDate = [[NSCalendar currentCalendar] dateFromComponents:startComponents];
-					
-					NSDateComponents *endComponents = [[NSDateComponents alloc] init];
-					[endComponents setDay:13];
-					[endComponents setMonth:7];
-					[endComponents setYear:2013];
-					[endComponents setHour:14];
-					[endComponents setMinute:0];
-					NSDate *endDate = [[NSCalendar currentCalendar] dateFromComponents:endComponents];
-					
-					[self promptToAddEventToCalendarWithTitle:@"Legends Pro-College Stroke & Best Ball Challenge" startDate:startDate endDate:endDate allDay:NO];
-					
-					break;
-				}
-				default:
-					break;
-			}
-			break;
-		case 2:
-			switch (indexPath.row) {
-				case 0:
-				{
-					NSDateComponents *startComponents = [[NSDateComponents alloc] init];
-					[startComponents setDay:14];
-					[startComponents setMonth:7];
-					[startComponents setYear:2013];
-					[startComponents setHour:7];
-					[startComponents setMinute:30];
-					NSDate *startDate = [[NSCalendar currentCalendar] dateFromComponents:startComponents];
-					
-					NSDateComponents *endComponents = [[NSDateComponents alloc] init];
-					[endComponents setDay:14];
-					[endComponents setMonth:7];
-					[endComponents setYear:2013];
-					[endComponents setHour:13];
-					[endComponents setMinute:30];
-					NSDate *endDate = [[NSCalendar currentCalendar] dateFromComponents:endComponents];
-					
-					[self promptToAddEventToCalendarWithTitle:@"Collegiate Stroke Play Second Round" startDate:startDate endDate:endDate allDay:NO];
-					
-					break;
-				}
-				default:
-					break;
-			}
-			break;
-		case 3:
-			switch (indexPath.row) {
-				case 0:
-				{
-					NSDateComponents *startComponents = [[NSDateComponents alloc] init];
-					[startComponents setDay:15];
-					[startComponents setMonth:7];
-					[startComponents setYear:2013];
-					[startComponents setHour:7];
-					[startComponents setMinute:30];
-					NSDate *startDate = [[NSCalendar currentCalendar] dateFromComponents:startComponents];
-					
-					NSDateComponents *endComponents = [[NSDateComponents alloc] init];
-					[endComponents setDay:15];
-					[endComponents setMonth:7];
-					[endComponents setYear:2013];
-					[endComponents setHour:13];
-					[endComponents setMinute:30];
-					NSDate *endDate = [[NSCalendar currentCalendar] dateFromComponents:endComponents];
-					
-					[self promptToAddEventToCalendarWithTitle:@"Collegiate Stroke Play Final Round" startDate:startDate endDate:endDate allDay:NO];
-					
-					break;
-				}
-				default:
-					break;
-			}
+			cell.textLabel.text = eventName;
+			cell.textLabel.font = [UIFont systemFontOfSize:15];
 			break;
 		default:
 			break;
 	}
+    
+    return cell;
+}
+
+- (NSString*) dateStringFromEvent:(NSDictionary*)event {
+	NSDate *date = [self dateFromEvent:event];
+	NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+	formatter.dateFormat = @"EEEE MMMM dd, yyyy hh:mm a";
+	return [formatter stringFromDate:date];
+}
+
+- (NSDate*) dateFromEvent:(NSDictionary*)event {
+	NSNumber *day = [event objectForKey:@"day"];
+	NSNumber *month = [event objectForKey:@"month"];
+	NSNumber *year = [event objectForKey:@"year"];
+	NSNumber *hour = [event objectForKey:@"hour"];
+	NSNumber *minute = [event objectForKey:@"minute"];
+	
+	NSDateComponents *components = [[NSDateComponents alloc] init];
+	[components setDay:day.intValue];
+	[components setMonth:month.intValue];
+	[components setYear:year.intValue];
+	[components setHour:hour.intValue];
+	[components setMinute:minute.intValue];
+	return [[NSCalendar currentCalendar] dateFromComponents:components];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	[super tableView:tableView didSelectRowAtIndexPath:indexPath];
+	
+	NSDictionary *event = [self.days objectAtIndex:indexPath.section];
+	
+	NSDate *startDate = [self dateFromEvent:[event objectForKey:@"startDate"]];
+	NSDate *endDate = [self dateFromEvent:[event objectForKey:@"endDate"]];
+	NSString *title = [event objectForKey:@"title"];
+	BOOL allDay = NO;
+	
+	[self promptToAddEventToCalendarWithTitle:title startDate:startDate endDate:endDate allDay:allDay];
 }
 
 @end
